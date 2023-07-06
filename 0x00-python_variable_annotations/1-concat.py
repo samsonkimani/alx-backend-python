3a4d1d45ae0693d5a0baf9f83ce93e7f4074aad5
samsonkimani <samsonkimani43@gmail.com>
2023-07-06 13:51:38 +0300

    a function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+a concat function that adds two strings
+@str1: the first string
+@str2: the second string
+
+return: a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ a function to concatenate two strings"""
+    return str1 + str2
