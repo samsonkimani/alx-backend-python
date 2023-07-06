7b41c8087c2dde813db4998ea45883f5d8de810f
samsonkimani <samsonkimani43@gmail.com>
2023-07-06 13:42:20 +0300

    an annotated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+a function add that returns the sum of two numbers
+the function should be type annotated
+
+@a: the first argument
+@b: the second argument
+"""
+
+
+def add(a: float, b: float) -> float:
+
+    return a + b
