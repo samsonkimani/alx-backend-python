3d449e034d0037a5505441008412920f4a41e154
samsonkimani <samsonkimani43@gmail.com>
2023-07-06 13:57:55 +0300

    a floor function that accepts a float and returns an integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+
+"""
+a function to return the floor of a number
+@n: the argument to pass
+return an int floor
+"""
+
+
+def floor(n: float) -> int:
+    """ a function that returns the floor of a number"""
+    return math.floor(n)
