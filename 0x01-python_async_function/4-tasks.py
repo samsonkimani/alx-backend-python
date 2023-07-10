4dcac60ba00402614f374e63210cb13bde4da80f
samsonkimani <samsonkimani43@gmail.com>
2023-07-10 19:54:16 +0300

    creating the task wait_n function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""a nearly identical wait_n function implementation"""
 
-"""
-"""
 
 import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """a modified implementation of wait_n
+    """
+
+    results = await asyncio.gather(*tuple(map(lambda _: task_wait_random(
+        max_delay), range(n))))
+    return sorted(results)
