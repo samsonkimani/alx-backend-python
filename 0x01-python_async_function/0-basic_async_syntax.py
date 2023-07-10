17dafca77bf037238f0ec75dba00e0f16c63203d
samsonkimani <samsonkimani43@gmail.com>
2023-07-10 18:55:20 +0300

    documenting module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-import random
-import asyncio
-
 """
 creating and async function that returns a delay
 @max_delay the maximum delay
 return: a random delay
 """
 
+import asyncio
+import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """wait for max_delay """
