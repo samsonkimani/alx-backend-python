853fb629f4d1c2f0f48b311ae1226af2f099405f
samsonkimani <samsonkimani43@gmail.com>
2023-07-10 19:22:07 +0300

    measuring the average runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+a function to measure runtime
+"""
+
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int):
+    """ """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    average_time = total_time / n
+
+    return average_time
