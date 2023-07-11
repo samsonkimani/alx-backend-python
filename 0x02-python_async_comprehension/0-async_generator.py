a254001d185e9206d7250279d970245dcd0f7a68
samsonkimani <samsonkimani43@gmail.com>
2023-07-11 15:31:48 +0300

    creating a generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,5 @@ import random
 async def async_generator():
     """ an async generator functio"""
     for _ in range(10):
-        i = random.uniform(0, 10)
-        yield i
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
