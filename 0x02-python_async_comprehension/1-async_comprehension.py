aae10db542779aee83dd78fba3971df527c90b65
samsonkimani <samsonkimani43@gmail.com>
2023-07-11 16:13:36 +0300

    type annotating

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,11 @@ write an async comprehension
 """
 
 import asyncio
+from typing import Generator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None]:
     """ async generator"""
     return [i async for i in async_generator()]
