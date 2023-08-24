3fe5a2c4a25c71c7c2f3fc053113a97bd29070c4
samsonkimani <samsonkimani43@gmail.com>
2023-08-24 19:35:37 +0300

    working with union and list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" using list and union"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''return sum of mxt_lst'''
+    return sum(mxd_lst)
