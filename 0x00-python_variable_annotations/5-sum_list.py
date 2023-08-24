00fee49c99a65a444fe657f3f770222b45180e89
samsonkimani <samsonkimani43@gmail.com>
2023-08-24 19:30:48 +0300

    using List to type annotate

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" woking with typing"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''return sum of list members'''
+    return sum(input_list)
